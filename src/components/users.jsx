3021ce8711158b80b6aea9fa2f923000160f2f78
Александр Пушкин <aleksandrpushkin@MacBook-Air-Aleksandr.local>
2022-07-05 16:44:07 +0300

    Initial commit

---
+++ src/components/users.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import api from '../api';
+
+const Users = () => {
+    const [users, setUsers] = useState(api.users.fetchAll());
+
+    const handleDelete = (userId) => {
+        setUsers(users.filter(user => user._id !== userId));
+    }
+
+    const renderPhrase = (number) => {
+        let message = 'Никто с тобой не тусанёт';
+        let classname = 'danger';
+
+        if (number !== 0) {
+            const template = 'с тобой сегодня';
+            message = number >= 2 && number <= 4
+                ? number.toString() + ' человека тусанут ' + template
+                : number.toString() + ' человек тусанёт ' + template;
+
+            classname = 'primary';
+        }
+
+        return <h3>
+            <span className={`badge bg-${classname}`}>
+                {message}
+            </span>
+        </h3>
+    }
+
+    return <>
+        {renderPhrase(users.length)}
+        {users.length !== 0 && <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(user => <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{user.qualities.map(quality => <span
+                        className={`badge bg-${quality.color} m-1`}
+                        key={quality._id}>
+                        {quality.name}
+                    </span>)}
+                    </td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}/5</td>
+                    <td><button
+                        onClick={() => handleDelete(user._id)}
+                        className='badge bg-danger border-0'>
+                        Delete
+                    </button></td>
+                </tr>)}
+            </tbody>
+        </table>}
+    </>
+};
+
+export default Users;
